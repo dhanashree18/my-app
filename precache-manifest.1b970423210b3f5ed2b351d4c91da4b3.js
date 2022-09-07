f59d54df712450b4fdbbb1077f36a3bc99df7810
Dhanashree <dbmane18@gmail.com>
2022-09-07 10:41:14 +0530

    Updates

---
+++ precache-manifest.1b970423210b3f5ed2b351d4c91da4b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a49f3bad3dbb99a5b748",
+    "url": "/my-app/static/css/main.dfe35daf.chunk.css"
+  },
+  {
+    "revision": "a49f3bad3dbb99a5b748",
+    "url": "/my-app/static/js/main.af5b3de4.chunk.js"
+  },
+  {
+    "revision": "cfa7322796517912f0d1",
+    "url": "/my-app/static/js/runtime~main.7a725f0a.js"
+  },
+  {
+    "revision": "d7552a1f1f052f900238",
+    "url": "/my-app/static/js/2.265c2a91.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/my-app/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "f757a3c2b9b189cf81275d3804240a44",
+    "url": "/my-app/index.html"
+  }
+];
